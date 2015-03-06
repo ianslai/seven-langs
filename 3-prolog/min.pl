a2acfc6416b7f09fe10d48ea15fb612c20c9d241
Ian Lai <ianslai@gmail.com>
2015-03-06 00:04:03 -0500

    Redo min to use my own logic

--- 3-prolog/min.pl
+++ 3-prolog/min.pl
@@ -1,4 +1,5 @@
-min(List, A) :- List = [H|_], min(List, H, A).
+min(List, A) :- min(List, [], A).
 min([], A, A).
+min([Head|Tail], [], Min) :- min(Tail, Head, Min).
 min([Head|Tail], Acc, Min) :- Head < Acc, min(Tail, Head, Min).
 min([Head|Tail], Acc, Min) :- Head >= Acc, min(Tail, Acc, Min).
