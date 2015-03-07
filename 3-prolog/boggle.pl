8d0f5f5e383a91d7c01ef2fe09bc37982ccdd881
Ian Lai <ianslai@gmail.com>
2015-03-07 07:20:29 -0500

    First attempt at Prolog Boggle.  It spews a path for each neighbor of the final cube and doesn't check for repeats.

---
+++ 3-prolog/boggle.pl
@@ -0,0 +1,44 @@
+
+board((1, 1), 'A').
+board((1, 2), 'B').
+board((1, 3), 'C').
+board((1, 4), 'D').
+board((2, 1), 'A').
+board((2, 2), 'B').
+board((2, 3), 'A').
+board((2, 4), 'Z').
+board((3, 1), 'A').
+board((3, 2), 'O').
+board((3, 3), 'G').
+board((3, 4), 'G').
+board((4, 1), 'A').
+board((4, 2), 'A').
+board((4, 3), 'E').
+board((4, 4), 'L').
+
+valid_cube(R, C) :- fd_domain(R, 1, 4), fd_domain(C, 1, 4).
+
+neighbor((R, C), (R1, C)) :- R1 is R + 1, valid_cube(R1, C).
+neighbor((R, C), (R, C1)) :- C1 is C + 1, valid_cube(R, C1).
+neighbor((R, C), (R1, C)) :- R1 is R - 1, valid_cube(R1, C).
+neighbor((R, C), (R, C1)) :- C1 is C - 1, valid_cube(R, C1).
+
+% Diagonals, for fun
+neighbor((R, C), (R1, C1)) :- R1 is R - 1, C1 is C - 1, valid_cube(R1, C1).
+neighbor((R, C), (R1, C1)) :- R1 is R - 1, C1 is C + 1, valid_cube(R1, C1).
+neighbor((R, C), (R1, C1)) :- R1 is R + 1, C1 is C - 1, valid_cube(R1, C1).
+neighbor((R, C), (R1, C1)) :- R1 is R + 1, C1 is C + 1, valid_cube(R1, C1).
+
+path([], [_|PathTail], PathTail).
+
+path([WordHead|WordTail], [PathHead|PathTail], Path) :-
+    board(PathHead, WordHead),
+    neighbor(PathHead, Next),
+    path(WordTail, [Next|[PathHead|PathTail]], Path).
+
+boggle_word(Word, Path) :-
+    atom_chars(Word, Chars),
+    valid_cube(R, C),
+    path(Chars, [(R, C)], Path).
+
+% boggle_word('BOGGLE', Path).
