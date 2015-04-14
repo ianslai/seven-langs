99359395a4704d7ccce4882a582e266abea771e9
Ian Lai <ian@kayak.com>
2015-04-13 21:57:52 -0400

    Clean up existing implementation

--- 5-erlang/day1.erl
+++ 5-erlang/day1.erl
@@ -1,7 +1,9 @@
 -module(day1).
 -export([count_words/1]).
 -export([count_to/1]).
+-export([match_success/1]).
 
+% ------------------------------------------------------------------------
 count_words(String) -> count_non_word_chars(String, 0).
 
 is_word_char(Char) ->
@@ -11,35 +13,43 @@ is_word_char(Char) ->
     true -> false
   end.
 
-count_non_word_chars(String, Count) ->
-  case String of
-    "" -> Count;
-    [Head|Tail] ->
-      IsWordChar = is_word_char(Head),
-      if
-        IsWordChar -> count_word_chars(Tail, Count + 1);
-        true -> count_non_word_chars(Tail, Count)
-      end
+count_non_word_chars("", Count) -> Count;
+count_non_word_chars([Head|Tail], Count) ->
+  IsWordChar = is_word_char(Head),
+  if
+    IsWordChar -> count_word_chars(Tail, Count + 1);
+    true -> count_non_word_chars(Tail, Count)
   end.
 
-count_word_chars(String, Count) ->
-  case String of
-    "" -> Count;
-    [Head|Tail] ->
-      IsWordChar = is_word_char(Head),
-      if
-        IsWordChar -> count_word_chars(Tail, Count);
-        true -> count_non_word_chars(Tail, Count)
-      end
+count_word_chars("", Count) -> Count;
+count_word_chars([Head|Tail], Count) ->
+  IsWordChar = is_word_char(Head),
+  if
+    IsWordChar -> count_word_chars(Tail, Count);
+    true -> count_non_word_chars(Tail, Count)
   end.
 
+% ------------------------------------------------------------------------
 
 count_to(Num) -> count_to(Num, 1).
 
 count_to(Num, Start) ->
   if 
-    Num /= Start ->
+    Num >= Start ->
       io:format("~p~n", [Start]),
       count_to(Num, Start + 1);
-    true -> Num
+    true -> ok
   end.
+
+% ------------------------------------------------------------------------
+
+% match_success(Input) ->
+%   case Input of
+%     success -> io:format("success~n");
+%     {error, Message} -> io:format("error: ~s~n", [Message])
+%   end.
+
+match_success(success) -> io:format("success~n");
+match_success({error, Message}) -> io:format("error: ~s~n", [Message]).
+
+% ------------------------------------------------------------------------
